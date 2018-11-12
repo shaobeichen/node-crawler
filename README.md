2535e6a8e4a7079e9487f27ff893a3e2dcc48757
leachzhou <leachzhou@163.com>
2018-11-12 17:34:37 +0800

    完成简单爬虫

--- README.md
+++
@@ -1,3 +0,0 @@
-# NodeDemo
-
-练习node途中的一些demo
--- index.js
+++ index.js
@@ -1,28 +1,33 @@
-const mysql = require('mysql');
-let connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'test'
+const eventproxy = require('eventproxy');
+const cheerio = require('cheerio');
+const express = require('express');
+const superagent = require('superagent');
+const app = express();
+var async = require('async');
+// url 模块是 Node.js 标准库里面的
+// http://nodejs.org/api/url.html
+const url = require('url');
+
+let cnodeUrl = 'https://cnodejs.org/';
+
+// 用 superagent 去抓取 https://cnodejs.org/ 的内容
+superagent.get(cnodeUrl).end(function (err, res) {
+    if (err) {
+        return console.error(err);
+    }
+    var topicUrls = [];
+    var $ = cheerio.load(res.text);
+    // 获取首页所有的链接
+    $('#topic_list .topic_title').each(function (idx, element) {
+        var $element = $(element);
+        // $element.attr('href') 本来的样子是 /topic/542acd7d5d28233425538b04
+        // 我们用 url.resolve 来自动推断出完整 url，变成
+        // https://cnodejs.org/topic/542acd7d5d28233425538b04 的形式
+        // 具体请看 http://nodejs.org/api/url.html#url_url_resolve_from_to 的示例
+        var href = url.resolve(cnodeUrl, $element.attr('href'));
+        topicUrls.push(href);
+    });
+
+    console.log(topicUrls);
 });
 
-connection.connect();
-
-let table = 'list';
-
-let selSql = `SELECT * FROM ${table}`;
-
-let addSql = `INSERT INTO ${table}(id,con) VALUES(0,?)`;
-let addSqlParams = ['菜鸟工具'];
-
-var modSql = `UPDATE ${table} SET con = ? WHERE id = ?`;
-var modSqlParams = ['菜鸟移动站', 26];
-
-var delSql = `DELETE FROM ${table} where id=26`;
-
-connection.query(addSql, addSqlParams, function (error, results, fields) {
-    if (error) throw error;
-    console.log(results);
-});
-
-connection.end();
